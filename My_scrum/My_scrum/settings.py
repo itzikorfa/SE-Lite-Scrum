e210c3113780062828d22f69d8b2a7be1dbf0ca1
itzikorfa <itzikorfa@users.noreply.github.com>
2017-07-13 22:58:41 +0300

    add user login form

--- My_scrum/My_scrum/settings.py
+++ My_scrum/My_scrum/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+LOGIN_REDIRECT_URL = '/company/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
