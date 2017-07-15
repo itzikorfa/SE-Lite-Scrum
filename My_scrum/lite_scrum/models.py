a800dc6a821f66cb9fb9bb89af8e0f1250fac4a9
itzikorfa <itzikorfa@users.noreply.github.com>
2017-07-15 21:43:10 +0300

    type of backlog

--- My_scrum/lite_scrum/models.py
+++ My_scrum/lite_scrum/models.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
 class Company(models.Model):
@@ -20,6 +20,10 @@ class Backlog(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
     timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
+    product_backlog = models.BooleanField(default=True)
+    release_backlog = models.BooleanField(default=False)
+    product_owner = models.ForeignKey(User, default=1,related_name='%(class)s_product_owner')
+    scrum_master = models.ForeignKey(User, default=1,related_name='%(class)s_scrum_master')
 
     def __str__(self):
         return "{} | {}".format(self.company, self.name)
