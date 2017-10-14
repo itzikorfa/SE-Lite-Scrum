5c63c3e28c2c7fde68996ff8ea0a5b588157528c
itzikorfa <iziaman@gmail.com>
2017-10-15 00:21:01 +0300

    add project

--- log/forms.py
+++ log/forms.py
@@ -8,11 +8,5 @@ class LogCreateViewForm4MProject(forms.ModelForm):
         model= Log
         fields = ('task', 'log', )
 
-    def __init__(self, *args, **kwargs):
-        # if 'task_id' in kwargs:
-        task_id=kwargs.pop('prj')
-        self.fields['task'].queryset = Task.objects.filter(pk=task_id)
-        # self.fields['task'].widget.attrs['readonly'] = True
-        super(LogCreateViewForm4MProject, self).__init__(self, *args, **kwargs)
 
 
--- log/views.py
+++ log/views.py
@@ -6,6 +6,8 @@ from django.views.generic import (View,TemplateView,
                                 CreateView,DeleteView,
                                 UpdateView)
 from . import models
+from task.models import TaskProperty
+from django.shortcuts import get_object_or_404
 from . import forms
 # Create your views here.
 
@@ -24,17 +26,28 @@ class LogCreateView(CreateView):
     fields = ("task",'log')
     model = models.Log
 
-class LogCreateView4MProject(CreateView):
+class LogCreateView4MTask(CreateView):
     model = models.Log
-    form_class = forms.LogCreateViewForm4MProject
+    # form_class = forms.LogCreateViewForm4MProject
+    fields = ('log',)
+
 
     def get_context_data(self, **kwargs):
-        test = kwargs.pop('prj')
+        test = self.kwargs.pop('pk')
         print(test)
-        context = super(LogCreateView4MProject, self).get_context_data(**kwargs)
-        context['prj']=self.prj
+        self.task = get_object_or_404(TaskProperty, pk=test)
+        context = super(LogCreateView4MTask, self).get_context_data(**kwargs)
+        context['pk']=test
         print(len(context), context)
         return context
+
+    def form_valid(self, form):
+        test = self.kwargs.pop('pk')
+        print(test)
+
+        form.instance.task = get_object_or_404(TaskProperty, pk=test)# success_url = reverse_lazy("project:blcreate")
+
+        return super(LogCreateView4MTask, self).form_valid(form)
 
 
 class LogUpdateView(UpdateView):
